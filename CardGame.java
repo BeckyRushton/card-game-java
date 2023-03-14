f254e932617a019a5d5a0725f781070a2fd1d724
Becky Rushton <beckyrushton30@gmail.com>
2023-03-14 14:47:21 +0000

    added deal card method that works in main

--- CardGame.java
+++ CardGame.java
@@ -13,11 +13,37 @@ public class CardGame {
         return deckOfCards;
     }
 
+    // Sorts the deck in number order (e.g. 2222333344445555 etc) and stores the new
+    // shuffled deck back into the deckOfCards attribute.
+    // Will need to sort by value as the only parameter and store it as a new list.
+    public ArrayList<Card> sortDeckInNumberOrder() {
+        return deckOfCards;
+    }
+
+    //  Sorts the deck into suits (2,3,4,5,6,7,8,9,10,J,Q,K,A of hearts,
+    //  then 2,3,4,5,6,7,8,9,10,J,Q,K,A of clubs etc) and stores the new shuffled
+    //  deck back into the deckOfCards attribute.
+    //  Similar to deckOfCards but will need to store new deck as a new list.
+    public ArrayList<Card> sortDeckIntoSuits() {
+        return deckOfCards;
+    }
+
+    //  Shuffles the deck into a random order and stores the new shuffled deck
+    //  back into the deckOfCards attribute.
+    //  Will need to store the new deck of cards as a new array list to be returned.
+    public ArrayList<Card> shuffleDeck() {
+        return deckOfCards;
+    }
+
     public void printDeck() {
         for (Card card : deckOfCards) {
             System.out.println(card.toString());
 
         }
+    }
+
+    public void dealCard() {
+            System.out.println(deckOfCards.get(0));
     }
 
     {
--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         CardGame cardGame = new CardGame("testing");
         cardGame.printDeck();
+        cardGame.dealCard();
+
     }
 }
